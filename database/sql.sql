aad3583a3e7084b5e462ad36a6f59c82242aeab8
Johny Welton <jwojs10@gmail.com>
2024-04-22 07:45:56 -0300

    exemplo k8s com pods rodando em namespace diferentes

---
+++ database/sql.sql
@@ -0,0 +1,8 @@
+CREATE TABLE mensagens (
+    id int,
+    nome varchar(50),
+    mensagem varchar(100)
+);
+
+INSERT INTO mensagens (id, nome, mensagem) VALUES (1,'Johny','Legal!!!...');
+INSERT INTO mensagens (id, nome, mensagem) VALUES (2,'Welton','Top!!');
